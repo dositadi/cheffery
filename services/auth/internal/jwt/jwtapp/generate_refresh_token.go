c5825f64265d9ecafb376bd6953939f6a9de1a70
dositadi <ositadinmadivine61@gmail.com>
2026-08-30 03:13:33 +0100

    added generate refresh token

--- services/auth/internal/jwt/jwtapp/generate_refresh_token.go
+++ services/auth/internal/jwt/jwtapp/generate_refresh_token.go
@@ -1,7 +1,55 @@
 package jwtapp
 
-import "context"
+import (
+	"context"
+	"time"
 
-func (u *Usecase) ExecuteGenerateRefreshToken(ctx context.Context, userId string) (string, error) {
-	return "", nil
+	"github.com/dositadi/cheffery/services/auth/internal/jwt/jwtdomain"
+	"github.com/dositadi/cheffery/services/shared/customerror"
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/google/uuid"
+)
+
+func (u *Usecase) ExecuteGenerateRefreshToken(ctx context.Context, reqId, userId string) (string, error) {
+	tokenId := uuid.NewString()
+	scope := "jwtapp.ExecuteGenerateAccessToken()"
+
+	claim := jwtdomain.JWTRefreshClaim{
+		UserID: userId,
+		RegisteredClaims: jwt.RegisteredClaims{
+			Issuer:    jwtdomain.Issuer.String(),
+			Subject:   userId,
+			Audience:  jwt.ClaimStrings{"cheffery:user", "cheffery:app"},
+			ExpiresAt: jwt.NewNumericDate(time.Now().Add(u.cfg.AccessTTL)),
+			NotBefore: jwt.NewNumericDate(time.Now()),
+			IssuedAt:  jwt.NewNumericDate(time.Now()),
+			ID:        tokenId,
+		},
+	}
+
+	token, err := jwt.NewWithClaims(jwt.SigningMethodHS256, claim).SignedString(u.cfg.AccessKey)
+	if err != nil {
+		u.logger.PrintError(err, reqId, customerror.InternalError{
+			Inner:   err,
+			Message: err.Error(),
+			Misc:    nil,
+		}.Error(), map[string]string{
+			"Context": scope,
+		})
+		return "", err
+	}
+
+	// Store the token
+	if err := u.store.StoreRefreshToken(ctx, reqId, tokenId, userId, time.Now().Add(u.cfg.RefreshTTL)); err != nil {
+		u.logger.PrintError(err, reqId, customerror.InternalError{
+			Inner:   err,
+			Message: err.Error(),
+			Misc:    nil,
+		}.Error(), map[string]string{
+			"Context": scope,
+		})
+		return "", err
+	}
+
+	return token, nil
 }
--- services/auth/internal/jwt/jwtdomain/store.go
+++ services/auth/internal/jwt/jwtdomain/store.go
@@ -1,8 +1,12 @@
 package jwtdomain
 
-import "context"
+import (
+	"context"
+	"time"
+)
 
 type TokenStore interface {
 	IncrementTokenVersion(ctx context.Context, reqID, userId string) error
 	GetTokenVersion(ctx context.Context, reqID, userID string) (int64, error)
+	StoreRefreshToken(ctx context.Context, reqID, tokenId, userId string, expiresAt time.Time) error
 }
