37fe9153d969882075601ea4192e4e87b2c16886
dositadi <ositadinmadivine61@gmail.com>
2026-09-02 23:18:24 +0100

    added paltform application starter

--- go.mod
+++ go.mod
@@ -3,11 +3,11 @@ module github.com/dositadi/cheffery
 go 1.26.7
 
 require (
-	github.com/go-chi/chi/v5 v5.3.2
 	github.com/golang-jwt/jwt/v5 v5.3.1
 	github.com/google/uuid v1.6.0
 	github.com/redis/go-redis/v9 v9.22.0
 	github.com/rs/zerolog v1.35.1
+	golang.org/x/sync v0.22.0
 	google.golang.org/grpc v1.83.2
 	google.golang.org/protobuf v1.36.12
 )
---
+++ services/auth/internal/platform/platformapp/app.go
@@ -0,0 +1,102 @@
+package platformapp
+
+import (
+	"fmt"
+	"net"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	gen "github.com/dositadi/cheffery/protoc_gen/protoc/auth"
+	"github.com/dositadi/cheffery/services/auth/internal/config"
+	"github.com/dositadi/cheffery/services/auth/internal/jwt/jwtapp"
+	"github.com/dositadi/cheffery/services/auth/internal/jwt/jwtserver"
+	"github.com/dositadi/cheffery/services/auth/internal/platform/rdb/rdbdomain"
+	"github.com/dositadi/cheffery/services/auth/internal/store"
+	"github.com/dositadi/cheffery/services/shared/customerror"
+	"github.com/dositadi/cheffery/services/shared/logger"
+	"golang.org/x/sync/errgroup"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+)
+
+type App struct {
+	cfg        *config.Config
+	logger     logger.Logger
+	redis      *rdbdomain.Redis
+	tokenStore *store.TokenStore
+	server     *jwtserver.Server
+	executor   *jwtapp.Usecase
+}
+
+func New() *App {
+	app := &App{}
+
+	app.logger = logger.New(os.Stdout)
+	app.cfg = config.New(app.logger)
+	app.redis = rdbdomain.New(app.logger, app.cfg.RedisConfig)
+	app.tokenStore = store.NewTokenStore(app.logger, app.redis.Client)
+	app.executor = jwtapp.New(app.logger, app.cfg.JWTConfig, app.tokenStore)
+	app.server = jwtserver.New(app.executor, app.logger, app.cfg.JWTConfig, app.tokenStore)
+
+	return app
+}
+
+func (a *App) startServer() {
+	scope := "platformapp.startServer"
+	reqId := "start-server"
+	// Listen to port
+	listener, err := net.Listen("tcp", ":50051")
+	if err != nil {
+		a.logger.PrintFatal(err, reqId, customerror.InternalError{
+			Inner:   err,
+			Message: fmt.Errorf("failed to listen: %w", err).Error(),
+			Misc:    nil,
+		}.Error(), map[string]string{
+			"Context": scope,
+		})
+	}
+
+	// Create a gRPC server
+	grpcServer := grpc.NewServer(grpc.ConnectionTimeout(time.Duration(2 * time.Second)))
+
+	// Register the service implementation to the protobuf
+	gen.RegisterIssuerServer(grpcServer, a.server)
+
+	// Enable reflection for debugging with tools like grpcurl.
+	reflection.Register(grpcServer)
+
+	chSignal := make(chan os.Signal, 1)
+	signal.Notify(chSignal, syscall.SIGINT, syscall.SIGTERM)
+
+	eg := new(errgroup.Group)
+
+	eg.Go(func() error {
+		if err := grpcServer.Serve(listener); err != nil {
+			return err
+		}
+		return nil
+	})
+
+	a.logger.PrintInfo(reqId,"starting auth gRPC server on: ")
+
+	if err := eg.Wait(); err != nil {
+		a.logger.PrintFatal(err, reqId, customerror.InternalError{
+			Inner:   err,
+			Message: fmt.Errorf("server stopped: %w", err).Error(),
+			Misc:    nil,
+		}.Error(), map[string]string{
+			"Context": scope,
+		})
+	}
+
+	<-chSignal
+
+	grpcServer.GracefulStop()
+	a.redis.Client.Close()
+}
+
+func (a *App) Run() {
+	a.startServer()
+}
